a295e8c8cdaad3a0253819b4beb1d62a8dc78828
Hydrogen <3038094028@qq.com>
2023-07-11 02:54:54 +0800

    chore: update test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,13 @@
 import { describe, expect, it } from 'vitest'
+import { openAsync, aria2, system } from '..'
+import { WebSocket } from 'isomorphic-ws'
 
-describe('hello', () => {
-  it('should work', () => {
-    expect('Hello').toEqual('Hello');
-  });
-});
+describe('aria2', async () => {
+  it('should work', async () => {
+    const socket = new WebSocket('ws://localhost:6800/jsonrpc')
+    const conn = await openAsync(socket)
+    expect(await aria2.getVersion(conn))
+      .property('enabledFeatures')
+      .satisfies(Array.isArray)
+  })
+})
