857f825ee463e151b9faf160c3242efc706c011a
Hydrogen <3038094028@qq.com>
2023-07-11 02:56:03 +0800

    feat: conn

--- src/conn.ts
+++ src/conn.ts
@@ -1,88 +1,28 @@
-import {
-  once,
-  type Disposable,
-  type MaybePromise,
-  type TrimStart,
-} from './utils'
-import WebSocket from 'isomorphic-ws'
+import { type Disposable } from './utils'
 export interface Socket {
+  /**
+   * Ready state of the socket.
+   * - `0` if the socket is connecting.
+   * - `1` if the socket is open.
+   * - `2` if the socket is closing or closed.
+   */
+  readyState: number
   send(data: string): void
-  on(type: 'message', listener: (event: { data: string }) => void): void
+  addEventListener(
+    type: 'message',
+    listener: (event: { data: any }) => void
+  ): void
+  addEventListener(
+    type: 'open',
+    listener: () => void,
+    options?: { once: boolean }
+  ): void
 }
 
 export interface Conn {
-  sendRequest<T>(method: string, ...args: any[]): MaybePromise<T>
+  sendRequest<T>(method: string, ...args: any[]): PromiseLike<T>
   onNotification<T extends (...args: unknown[]) => void>(
     type: string,
     listener: T
   ): Disposable<T>
-}
-
-export const useSocket = (
-  socket: Socket,
-  secret?: string,
-  onerror?: (err: unknown) => void
-) => {
-  const e = new Map<string, Set<(...args: any[]) => void>>()
-  const c = new Map<string, (err?: any, ret?: any) => void>()
-  const u = (crypto ?? require('crypto').webcrypto).randomUUID
-  const n =
-    (id: string, r: (v: any) => void, e: (e: any) => void) =>
-    (err: any, ret: any) => {
-      c.delete(id)
-      if (err !== null) r(ret)
-      else e(err)
-    }
-
-  socket.on('message', ({ data }) => {
-    if (typeof data != 'string') return
-    try {
-      const j = JSON.parse(data)
-      if (j.method !== null) {
-        e.get(j.method)?.forEach((e) => e(...j.params))
-      } else if (j.id !== null) {
-        c.get(j.id)?.(j.error, j.result)
-      }
-    } catch (err: any) {
-      onerror?.(err)
-    }
-  })
-  return {
-    sendRequest: (method: string, ...params: any[]) =>
-      new Promise((r, e) => {
-        try {
-          const id = u()
-          params = secret !== null ? [secret, params] : params
-
-          c.set(id, n(id, r, e))
-
-          socket.send(
-            JSON.stringify({
-              jsonrpc: '2.0',
-              id,
-              method,
-              params,
-            })
-          )
-        } catch (err: any) {
-          onerror?.(err)
-        }
-      }),
-
-    onNotification: <T extends (...args: unknown[]) => void>(
-      type: string,
-      listener: T
-    ): Disposable<T> => {
-      let b = e.get(type)
-      b || e.set(type, (b = new Set()))
-      b.add(listener)
-
-      return {
-        dispose: once(() => {
-          e.get(type)?.delete(listener)
-          return listener
-        }),
-      }
-    },
-  }
 }
