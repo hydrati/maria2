8cd96efe2d344d6d2b759adb60dec36d2c23f666
Hydrogen <3038094028@qq.com>
2023-07-11 14:56:25 +0800

    fix(tests): missing crypto

--- tests/utils/global.ts
+++ tests/utils/global.ts
@@ -1,6 +1,8 @@
 import WebSocket from 'ws'
 import { vi, beforeAll } from 'vitest'
+import { webcrypto } from 'crypto'
 
 beforeAll(() => {
   vi.stubGlobal('WebSocket', WebSocket)
+  vi.stubGlobal('crypto', webcrypto)
 })
