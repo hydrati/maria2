3a7db601290ef450678d97d66a00b2e3f966f460
Hydrogen <3038094028@qq.com>
2023-07-12 02:39:59 +0800

    fix: test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,20 +2,20 @@ import { describe, expect, it } from 'vitest'
 
 describe('aria2', async () => {
   it('createWebSocket should work', async () => {
-    const { openAsync, createWebSocket, aria2, system } = await import('..')
+    const { open, createWebSocket, aria2, system } = await import('..')
 
     const socket = createWebSocket('ws://localhost:6800/jsonrpc')
-    const conn = await openAsync(socket)
+    const conn = await open(socket)
     expect(await aria2.getVersion(conn))
       .property('enabledFeatures')
       .satisfies(Array.isArray)
   })
 
   it('createHTTP should work', async () => {
-    const { openAsync, createHTTP, aria2, system } = await import('..')
+    const { open, createHTTP, aria2, system, close } = await import('..')
 
     const socket = createHTTP('http://localhost:6800/jsonrpc')
-    const conn = await openAsync(socket)
+    const conn = await open(socket)
     expect(await aria2.getVersion(conn))
       .property('enabledFeatures')
       .satisfies(Array.isArray)
