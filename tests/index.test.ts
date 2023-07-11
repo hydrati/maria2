d92663843f42a967992e682f25edeaa3d4023422
Hydrogen <3038094028@qq.com>
2023-07-11 14:32:38 +0800

    fix: test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,10 +1,12 @@
 import { describe, expect, it } from 'vitest'
 import { openAsync, createWebSocket, aria2, system } from '..'
+import { WebSocket } from 'ws'
+;(globalThis as any).WebSocket = WebSocket
 
 describe('aria2', async () => {
   it('should work', async () => {
     const socket = createWebSocket('ws://localhost:6800/jsonrpc')
-    const conn = await openAsync(socket)
+    const conn = await openAsync(socket, '123456')
     expect(await aria2.getVersion(conn))
       .property('enabledFeatures')
       .satisfies(Array.isArray)
