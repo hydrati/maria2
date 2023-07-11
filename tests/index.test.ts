38b7ca4b0197124ddc46bd6549a3e48dfe7eaa52
Hydrogen <3038094028@qq.com>
2023-07-11 13:27:58 +0800

    chore: test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,10 +1,9 @@
 import { describe, expect, it } from 'vitest'
-import { openAsync, aria2, system } from '..'
-import { WebSocket } from 'isomorphic-ws'
+import { openAsync, createWebSocket, aria2, system } from '..'
 
 describe('aria2', async () => {
   it('should work', async () => {
-    const socket = new WebSocket('ws://localhost:6800/jsonrpc')
+    const socket = createWebSocket('ws://localhost:6800/jsonrpc')
     const conn = await openAsync(socket)
     expect(await aria2.getVersion(conn))
       .property('enabledFeatures')
