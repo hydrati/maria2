9f9e68a99fcf863d2d8ad6afa3d36567483e445b
XLor <yjl9903@vip.qq.com>
2023-07-12 01:31:39 +0800

    feat: add socket ReadyState enum

--- src/conn.ts
+++ src/conn.ts
@@ -1,13 +1,25 @@
 import { type Disposable } from './utils'
+
+/**
+ * Ready state of the socket.
+ */
+export enum ReadyState {
+  Connecting = 0,
+  Open = 1,
+  Closing = 2,
+  Closed = 3
+}
+
 export interface Socket {
   /**
    * Ready state of the socket.
    * - `0` if the socket is connecting.
    * - `1` if the socket is open.
-   * - `2` if the socket is closing or closed.
+   * - `2` if the socket is closing.
+   * - `3` if the socket is closed.
    * @public
    */
-  readyState: number
+  readyState: ReadyState
 
   send(data: string): void
 
