deabde6382d04232f3a31f8c2fc557a199c9a746
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-07-12 02:11:09 +0800

    fix: rename `openAsync` to `open`

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,10 +2,10 @@ import { describe, expect, it } from 'vitest'
 
 describe('aria2', async () => {
   it('createWebSocket should work', async () => {
-    const { openAsync, createWebSocket, aria2, system } = await import('..')
+    const { open, createWebSocket, aria2, system } = await import('..')
 
     const socket = createWebSocket('ws://localhost:6800/jsonrpc')
-    const conn = await openAsync(socket)
+    const conn = await open(socket)
     expect(await aria2.getVersion(conn))
       .property('enabledFeatures')
       .satisfies(Array.isArray)
