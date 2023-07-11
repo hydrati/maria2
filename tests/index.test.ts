6e3136ccc0782b2b35f69fbf1d8d2e1202e997e9
Hydrogen <3038094028@qq.com>
2023-07-12 03:30:04 +0800

    chore: tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,50 @@
-import { describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
 
-describe('aria2', async () => {
+describe('without global', async () => {
+  beforeEach(() => {
+    vi.stubGlobal('WebSocket', void 0)
+    vi.stubGlobal('fetch', void 0)
+    vi.stubGlobal('crypto', void 0)
+  })
+
+  it('createWebSocket should work', async () => {
+    const { open, close, createWebSocket, aria2, ReadyState } = await import(
+      '../src'
+    )
+
+    const socket = createWebSocket('ws://localhost:6800/jsonrpc')
+    const conn = await open(socket)
+    expect(await aria2.getVersion(conn))
+      .property('enabledFeatures')
+      .satisfies(Array.isArray)
+
+    close(conn)
+    expect(socket.readyState).oneOf([ReadyState.Closing, ReadyState.Closed])
+  })
+
+  it('createHTTP should work', async () => {
+    const { open, createHTTP, aria2, system, close, ReadyState } = await import(
+      '../src'
+    )
+
+    const socket = createHTTP('http://localhost:6800/jsonrpc')
+    const conn = await open(socket)
+    expect(await aria2.getVersion(conn))
+      .property('enabledFeatures')
+      .satisfies(Array.isArray)
+
+    close(conn)
+
+    expect(socket.readyState).oneOf([ReadyState.Closing, ReadyState.Closed])
+  })
+})
+
+describe('normal', async () => {
+  beforeEach(async () => {
+    vi.stubGlobal('WebSocket', (await import('ws')).default)
+    vi.stubGlobal('fetch', (await import('cross-fetch')).fetch)
+  })
+
   it('createWebSocket should work', async () => {
     const { open, close, createWebSocket, aria2, ReadyState } = await import(
       '../src'
