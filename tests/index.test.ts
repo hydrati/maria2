f42152dc6949a892c3af507ba6f641e3e53867de
Hydrogen <3038094028@qq.com>
2023-07-13 13:00:41 +0800

    chore: tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,53 +1,12 @@
 import { beforeEach, describe, expect, it, vi } from 'vitest'
 
-describe('without global', async () => {
-  beforeEach(() => {
-    vi.stubGlobal('WebSocket', void 0)
-    vi.stubGlobal('fetch', void 0)
-    vi.stubGlobal('crypto', void 0)
-  })
-
+describe('maria2', async () => {
   it('createWebSocket should work', async () => {
     const { open, close, aria2, ReadyState } = await import('../src')
     const { createWebSocket } = await import('../src/transport')
 
     const socket = createWebSocket('ws://localhost:6800/jsonrpc')
-    const conn = await open(socket)
-    expect(await aria2.getVersion(conn))
-      .property('enabledFeatures')
-      .satisfies(Array.isArray)
-
-    close(conn)
-    expect(socket.readyState).oneOf([ReadyState.Closing, ReadyState.Closed])
-  })
-
-  it('createHTTP should work', async () => {
-    const { open, aria2, system, close, ReadyState } = await import('../src')
-    const { createHTTP } = await import('../src/transport')
-
-    const socket = createHTTP('http://localhost:6800/jsonrpc')
-    const conn = await open(socket)
-    expect(await aria2.getVersion(conn))
-      .property('enabledFeatures')
-      .satisfies(Array.isArray)
-
-    close(conn)
-
-    expect(socket.readyState).oneOf([ReadyState.Closing, ReadyState.Closed])
-  })
-})
-
-describe('normal', async () => {
-  beforeEach(async () => {
-    vi.stubGlobal('WebSocket', (await import('ws')).default)
-  })
-
-  it('createWebSocket should work', async () => {
-    const { open, close, aria2, ReadyState } = await import('../src')
-    const { createWebSocket } = await import('../src/transport')
-
-    const socket = createWebSocket('ws://localhost:6800/jsonrpc')
-    const conn = await open(socket)
+    const conn = await open(socket, { secret: '123456' })
 
     expect(await aria2.getVersion(conn))
       .property('enabledFeatures')
@@ -62,6 +21,40 @@ describe('normal', async () => {
     const { createHTTP } = await import('../src/transport')
 
     const socket = createHTTP('http://localhost:6800/jsonrpc')
+    const conn = await open(socket, { secret: '123456' })
+    expect(await aria2.getVersion(conn))
+      .property('enabledFeatures')
+      .satisfies(Array.isArray)
+
+    close(conn)
+
+    expect(socket.readyState).oneOf([ReadyState.Closing, ReadyState.Closed])
+  })
+
+  it('createWebSocket(pre) should work', async () => {
+    const { open, close, aria2, ReadyState } = await import('../src')
+    const { createWebSocket } = await import('../src/transport')
+
+    const socket = createWebSocket('ws://localhost:6800/jsonrpc', {
+      secret: '123456',
+    })
+    const conn = await open(socket)
+
+    expect(await aria2.getVersion(conn))
+      .property('enabledFeatures')
+      .satisfies(Array.isArray)
+
+    close(conn)
+    expect(socket.readyState).oneOf([ReadyState.Closing, ReadyState.Closed])
+  })
+
+  it('createHTTP(pre) should work', async () => {
+    const { open, close, aria2, ReadyState } = await import('../src')
+    const { createHTTP } = await import('../src/transport')
+
+    const socket = createHTTP('http://localhost:6800/jsonrpc', {
+      secret: '123456',
+    })
     const conn = await open(socket)
     expect(await aria2.getVersion(conn))
       .property('enabledFeatures')
